ee1c4b156f82a401e957614bac4c8ee7abe81fcc
franciscojarmison <phantomrising@protonmail.com>
2025-02-17 18:40:02 -0300

    add new features calculator sum,div,root,media

--- APIASPNET/APIASPNET/Controllers/CalculatorController.cs
+++ APIASPNET/APIASPNET/Controllers/CalculatorController.cs
@@ -14,10 +14,53 @@ namespace APIASPNET.Controllers
         }
 
         [HttpGet(Name = "sum/{firstNumber}/{secoundNumber}")]
-        public IActionResult Get(string firstNumber, string secoundNumber) {
+        public IActionResult Sum(string firstNumber, string secoundNumber) {
             if (IsNumeric(firstNumber) && IsNumeric(secoundNumber)) {
                 var sum = ConnvertToDecimal(firstNumber) + ConnvertToDecimal(secoundNumber);
                 return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet(Name ="sub/{firstNumber}/{secoundNumber}")]
+        public IActionResult Sub(string firstNumber, string secoundNumber) {
+            if (IsNumeric(firstNumber) && IsNumeric(secoundNumber)) {
+                var sub = ConnvertToDecimal(firstNumber) / ConnvertToDecimal(secoundNumber);
+                return Ok(sub.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet(Name = "mult/{firstNumber}/{secoundNumber}")]
+        public IActionResult Mult(string firstNumber, string secoundNumber) {
+            if (IsNumeric(firstNumber) && IsNumeric(secoundNumber))
+            {
+                var mult = ConnvertToDecimal(firstNumber) * ConnvertToDecimal(secoundNumber);
+                return Ok(mult.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet(Name = "med/{firstNumber}/{secoundNumber}")]
+        public IActionResult Med(string firstNumber, string secoundNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secoundNumber))
+            {
+                var med = (ConnvertToDecimal(firstNumber) + ConnvertToDecimal(secoundNumber)) / 2;
+                return Ok(med.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet(Name = "root/{firstNumber}/{secoundNumber}")]
+        public IActionResult Root(string firstNumber) {
+            if (IsNumeric(firstNumber)){
+                var med = (Math.Sqrt((double)ConnvertToDecimal(firstNumber)));
+                return Ok(med.ToString());
             }
 
             return BadRequest("Invalid Input");
